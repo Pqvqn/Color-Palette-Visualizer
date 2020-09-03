63b35be1eed767c7800aa268489e3a4eb3912d9d
Pqvqn <pavanarauch@hotmail.com>
2020-09-03 11:31:09 -0400

    prints out every color in image and the number of pixels with that color

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-import java.awt.Image;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
@@ -9,16 +13,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Map<Integer,Integer> colors = new HashMap<Integer,Integer>();
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Filepath: ");
-		String filepath = sc.nextLine();
+		String homepath = System.getProperty("user.home")+ "\\";
+		System.out.print("Filepath: "+homepath);
+		String filepath = homepath+sc.nextLine();
 		System.out.println();
 		sc.close();
+		BufferedImage img = null;
 		try {
-			Image i = ImageIO.read(new File(filepath));
+			img = ImageIO.read(new File(filepath));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		for(int i=0; i<img.getWidth(null); i++) {
+			for(int j=0; j<img.getHeight(null); j++) {
+				int rgb = img.getRGB(i,j);
+				if(!colors.containsKey(rgb))colors.put(rgb,0);
+				colors.put(rgb,colors.get(rgb)+1);
+			}
+		}
+		Iterator<Integer> it = colors.keySet().iterator();
+		while(it.hasNext()) {
+			Integer rgb = it.next();
+			int red = (rgb >> 16) & 0xFF;
+			int green = (rgb >> 8) & 0xFF;
+			int blue = rgb & 0xFF;
+			System.out.println("("+red+","+green+","+blue+") : "+colors.get(rgb));
+			//Color c = new Color(red,green,blue);
 		}
 		
 	}
