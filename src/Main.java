2381690d1c68794e665a934339f128b8cea932c7
Pqvqn <pavanarauch@hotmail.com>
2020-09-05 13:20:37 -0400

    implemented rudimentary color distance method

--- src/Main.java
+++ src/Main.java
@@ -71,6 +71,16 @@ public class Main {
 			}
 		}
 		
+		for(int i=0; i<6; i++) {
+			Color col1 = convertColor(sorted.get((int)(Math.random()*sorted.size())));
+			Color col2 = convertColor(sorted.get((int)(Math.random()*sorted.size())));
+			
+			System.out.print("("+col1.getRed()+","+col1.getGreen()+","+col1.getBlue()+")" );
+			System.out.print("("+col2.getRed()+","+col2.getGreen()+","+col2.getBlue()+")" );
+			System.out.println(colorsDistance(col1,col2));
+		}
+		
+		
 		System.out.print("Filepath: "+homepath);
 		String filepath_save = homepath+sc.nextLine();
 		System.out.println();
@@ -91,12 +101,37 @@ public class Main {
 	}
 
 
-	private static Color convertColor(int rgb) {
+	public static Color convertColor(int rgb) {
 		int red = (rgb >> 16) & 0xFF;
 		int green = (rgb >> 8) & 0xFF;
 		int blue = rgb & 0xFF;
 		Color c = new Color(red,green,blue);
 		return c;
 	
+	}
+	
+	public static boolean colorsClose(Color c1, Color c2, double cut) {
+		return colorsDistance(c1,c2)<cut;
+	}
+	public static boolean colorsClose(int c1, int c2, double cut) {
+		return colorsDistance(c1,c2)<cut;
+	}
+	public static boolean colorsClose(int r1, int g1, int b1, int r2, int g2, int b2, double cut) {
+		return colorsDistance(r1,g1,b1,r2,g2,b2) < cut;
+	}
+	public static double colorsDistance(Color c1, Color c2) {
+		return colorsDistance(c1.getRed(), c1.getGreen(), c1.getBlue(), c2.getRed(), c2.getGreen(), c2.getBlue());
+	}
+	public static double colorsDistance(int c1, int c2) {
+		int red1 = (c1 >> 16) & 0xFF;
+		int green1 = (c1 >> 8) & 0xFF;
+		int blue1 = c1 & 0xFF;
+		int red2 = (c2 >> 16) & 0xFF;
+		int green2 = (c2 >> 8) & 0xFF;
+		int blue2 = c2 & 0xFF;
+		return colorsDistance(red1,green1,blue1,red2,green2,blue2);
+	}
+	public static double colorsDistance(int r1, int g1, int b1, int r2, int g2, int b2) {
+		return Math.sqrt(Math.pow((r1-r2),2)+Math.pow((g1-g2),2)+Math.pow((b1-b2),2));
 	}
 }
