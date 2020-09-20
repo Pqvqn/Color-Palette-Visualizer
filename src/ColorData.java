af376f3faf51eec9a9b9ef39246891d2ef7e98ed
Pqvqn <pavanarauch@hotmail.com>
2020-09-19 20:35:02 -0400

    began color data storage classes

--- src/ColorData.java
+++ src/ColorData.java
@@ -1,4 +1,14 @@
 
 public class ColorData {
 
+	public int rgb; //color integer value
+	public int prevalence; //amount of pixels that are this exact color
+	
+	public ColorData(int rgb) {
+		this.rgb = rgb;
+		prevalence = 0;
+	}
+	
+	
+	
 }
